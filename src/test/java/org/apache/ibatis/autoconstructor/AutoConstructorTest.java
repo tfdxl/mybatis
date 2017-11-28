36f6cd86399b96cb3fb49c9f0b6a213f4e9cb534
tianfeng <tianfeng@souche.com>
2017-11-28 15:33:30 +0800

    format

--- src/test/java/org/apache/ibatis/autoconstructor/AutoConstructorTest.java
+++ src/test/java/org/apache/ibatis/autoconstructor/AutoConstructorTest.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2017 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2017 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.autoconstructor;
 
@@ -26,90 +26,95 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.PrintWriter;
 import java.io.Reader;
 import java.sql.Connection;
 import java.util.List;
 
 public class AutoConstructorTest {
-  private static SqlSessionFactory sqlSessionFactory;
 
-  @BeforeClass
-  public static void setUp() throws Exception {
-    // create a SqlSessionFactory
-    final Reader reader = Resources.getResourceAsReader("org/apache/ibatis/autoconstructor/mybatis-config.xml");
-    sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-    reader.close();
+    private static SqlSessionFactory sqlSessionFactory;
 
-    // populate in-memory database
-    final SqlSession session = sqlSessionFactory.openSession();
-    final Connection conn = session.getConnection();
-    final Reader dbReader = Resources.getResourceAsReader("org/apache/ibatis/autoconstructor/CreateDB.sql");
-    final ScriptRunner runner = new ScriptRunner(conn);
-    runner.setLogWriter(null);
-    runner.runScript(dbReader);
-    conn.close();
-    dbReader.close();
-    session.close();
-  }
+    @BeforeClass
+    public static void setUp() throws Exception {
+        // create a SqlSessionFactory
+        final Reader reader = Resources.getResourceAsReader("org/apache/ibatis/autoconstructor/mybatis-config.xml");
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        reader.close();
 
-  @Test
-  public void fullyPopulatedSubject() {
-    final SqlSession sqlSession = sqlSessionFactory.openSession();
-    try {
-      final AutoConstructorMapper mapper = sqlSession.getMapper(AutoConstructorMapper.class);
-      final Object subject = mapper.getSubject(1);
-      Assert.assertNotNull(subject);
-    } finally {
-      sqlSession.close();
+        // populate in-memory database
+        final SqlSession session = sqlSessionFactory.openSession();
+        final Connection conn = session.getConnection();
+        final Reader dbReader = Resources.getResourceAsReader("org/apache/ibatis/autoconstructor/CreateDB.sql");
+        final ScriptRunner runner = new ScriptRunner(conn);
+
+        runner.setLogWriter(new PrintWriter(System.out));
+
+        runner.runScript(dbReader);
+
+        conn.close();
+        dbReader.close();
+        session.close();
     }
-  }
 
-  @Test(expected = PersistenceException.class)
-  public void primitiveSubjects() {
-    final SqlSession sqlSession = sqlSessionFactory.openSession();
-    try {
-      final AutoConstructorMapper mapper = sqlSession.getMapper(AutoConstructorMapper.class);
-      mapper.getSubjects();
-    } finally {
-      sqlSession.close();
+    @Test
+    public void fullyPopulatedSubject() {
+        final SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            final AutoConstructorMapper mapper = sqlSession.getMapper(AutoConstructorMapper.class);
+            final Object subject = mapper.getSubject(1);
+            Assert.assertNotNull(subject);
+        } finally {
+            sqlSession.close();
+        }
     }
-  }
 
-  @Test
-  public void wrapperSubject() {
-    final SqlSession sqlSession = sqlSessionFactory.openSession();
-    try {
-      final AutoConstructorMapper mapper = sqlSession.getMapper(AutoConstructorMapper.class);
-      verifySubjects(mapper.getWrapperSubjects());
-    } finally {
-      sqlSession.close();
+    @Test(expected = PersistenceException.class)
+    public void primitiveSubjects() {
+        final SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            final AutoConstructorMapper mapper = sqlSession.getMapper(AutoConstructorMapper.class);
+            mapper.getSubjects();
+        } finally {
+            sqlSession.close();
+        }
     }
-  }
 
-  @Test
-  public void annotatedSubject() {
-    final SqlSession sqlSession = sqlSessionFactory.openSession();
-    try {
-      final AutoConstructorMapper mapper = sqlSession.getMapper(AutoConstructorMapper.class);
-      verifySubjects(mapper.getAnnotatedSubjects());
-    } finally {
-      sqlSession.close();
+    @Test
+    public void wrapperSubject() {
+        final SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            final AutoConstructorMapper mapper = sqlSession.getMapper(AutoConstructorMapper.class);
+            verifySubjects(mapper.getWrapperSubjects());
+        } finally {
+            sqlSession.close();
+        }
     }
-  }
 
-  @Test(expected = PersistenceException.class)
-  public void badSubject() {
-    final SqlSession sqlSession = sqlSessionFactory.openSession();
-    try {
-      final AutoConstructorMapper mapper = sqlSession.getMapper(AutoConstructorMapper.class);
-      mapper.getBadSubjects();
-    } finally {
-      sqlSession.close();
+    @Test
+    public void annotatedSubject() {
+        final SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            final AutoConstructorMapper mapper = sqlSession.getMapper(AutoConstructorMapper.class);
+            verifySubjects(mapper.getAnnotatedSubjects());
+        } finally {
+            sqlSession.close();
+        }
     }
-  }
 
-  private void verifySubjects(final List<?> subjects) {
-    Assert.assertNotNull(subjects);
-    Assertions.assertThat(subjects.size()).isEqualTo(3);
-  }
+    @Test(expected = PersistenceException.class)
+    public void badSubject() {
+        final SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            final AutoConstructorMapper mapper = sqlSession.getMapper(AutoConstructorMapper.class);
+            mapper.getBadSubjects();
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    private void verifySubjects(final List<?> subjects) {
+        Assert.assertNotNull(subjects);
+        Assertions.assertThat(subjects.size()).isEqualTo(3);
+    }
 }
