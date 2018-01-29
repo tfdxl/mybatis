17bcd3f31002de718181e8affbe152841190b205
tianfeng <tianfeng@souche.com>
2018-01-29 15:36:04 +0800

    format

--- src/test/java/org/apache/ibatis/binding/WrongNamespacesTest.java
+++ src/test/java/org/apache/ibatis/binding/WrongNamespacesTest.java
@@ -21,13 +21,13 @@ import org.junit.Test;
 public class WrongNamespacesTest {
 
     @Test(expected = RuntimeException.class)
-    public void shouldFailForWrongNamespace() throws Exception {
+    public void shouldFailForWrongNamespace() {
         Configuration configuration = new Configuration();
         configuration.addMapper(WrongNamespaceMapper.class);
     }
 
     @Test(expected = RuntimeException.class)
-    public void shouldFailForMissingNamespace() throws Exception {
+    public void shouldFailForMissingNamespace() {
         Configuration configuration = new Configuration();
         configuration.addMapper(MissingNamespaceMapper.class);
     }
