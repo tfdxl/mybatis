419a558c73fd9c8fde3bb5586d84f241642a35fa
tianfeng <tianfeng@souche.com>
2018-01-29 10:16:21 +0800

    format

--- src/main/java/org/apache/ibatis/annotations/DeleteProvider.java
+++ src/main/java/org/apache/ibatis/annotations/DeleteProvider.java
@@ -28,6 +28,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface DeleteProvider {
+
     Class<?> type();
 
     String method();
