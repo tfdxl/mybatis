eede12efae0314be9d697b73f50554bd40925434
tianfeng <tianfeng@souche.com>
2018-01-31 15:00:17 +0800

    add

---
+++ src/test/java/org/apache/ibatis/jdbc/Example.java
@@ -0,0 +1,73 @@
+package org.apache.ibatis.jdbc;
+
+import java.sql.*;
+
+public class Example {
+
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/emp";
+
+    //  Database credentials
+    static final String USER = "root";
+    static final String PASS = "123456";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            //STEP 1: 注册驱动
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //STEP 2: 打开数据库连接
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //STEP 3: 执行语句
+            stmt = conn.createStatement();
+            String sql = "SELECT id, first, last, age FROM Employees";
+
+            //STEP 4: 处理结果集
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+
+                //Retrieve by column name
+                int id = rs.getInt("id");
+                int age = rs.getInt("age");
+                String first = rs.getString("first");
+                String last = rs.getString("last");
+
+                //Display values
+                System.out.print("ID: " + id);
+                System.out.print(", Age: " + age);
+                System.out.print(", First: " + first);
+                System.out.println(", Last: " + last);
+            }
+
+            //STEP 5:释放资源
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+            }// nothing we can do
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        System.out.println("There are some thing wrong!");
+    }//end main
+}
