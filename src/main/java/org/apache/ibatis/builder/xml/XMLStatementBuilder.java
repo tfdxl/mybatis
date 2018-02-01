cfb5c06b71bfbc739ee6ee157a1384305c0b8498
tianfeng <tianfeng@souche.com>
2018-02-01 14:09:38 +0800

    refactor

--- src/main/java/org/apache/ibatis/builder/xml/XMLStatementBuilder.java
+++ src/main/java/org/apache/ibatis/builder/xml/XMLStatementBuilder.java
@@ -159,7 +159,7 @@ public class XMLStatementBuilder extends BaseBuilder {
 
         id = builderAssistant.applyCurrentNamespace(id, false);
 
-        MappedStatement keyStatement = configuration.getMappedStatement(id, false);
+        final MappedStatement keyStatement = configuration.getMappedStatement(id, false);
         configuration.addKeyGenerator(id, new SelectKeyGenerator(keyStatement, executeBefore));
     }
 
